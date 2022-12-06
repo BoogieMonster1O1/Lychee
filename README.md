9e460788b8ada9eb4a868fc48a061c3cd155f1c9
Shrish Deshpande <shrish.deshpande@icloud.com>
2022-12-06 15:12:03 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lychee
+
+A description of this package.
---
+++ Tests/LycheeTests/LycheeTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Lychee
+
+final class LycheeTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Lychee().text, "Hello, World!")
+    }
+}
